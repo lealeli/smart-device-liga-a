6726fade62f54dafbdcfa0d1976cc64a9d6a3a30
Elena <elenamitchenko@yandex.ru>
2021-03-23 16:05:06 +0300

    Правки по критериям

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,13 @@
-let menu = document.querySelector('.main-nav__toggle');
-let subMenu = document.querySelector('.main-nav__list');
+'use strict';
 
-const clickMenu = () => {
+var menu = document.querySelector('.main-nav__toggle');
+var subMenu = document.querySelector('.main-nav__list');
+
+subMenu.classList.remove('main-nav__list--nojs');
+
+function clickMenu() {
   menu.classList.toggle('main-nav__toggle--open');
   subMenu.classList.toggle('main-nav__list--close');
-};
+}
 menu.addEventListener('click', clickMenu);
 clickMenu();

15562c11b1e4a9604ef98ffff95a603493e0d58a
Elena <elenamitchenko@yandex.ru>
2021-03-24 08:04:22 +0300

    Правки CR

--- source/js/script.js
+++ source/js/script.js
@@ -3,11 +3,13 @@
 var menu = document.querySelector('.main-nav__toggle');
 var subMenu = document.querySelector('.main-nav__list');
 
-subMenu.classList.remove('main-nav__list--nojs');
-
 function clickMenu() {
   menu.classList.toggle('main-nav__toggle--open');
   subMenu.classList.toggle('main-nav__list--close');
 }
-menu.addEventListener('click', clickMenu);
-clickMenu();
+
+if (menu && subMenu) {
+  subMenu.classList.remove('main-nav__list--nojs');
+  menu.addEventListener('click', clickMenu);
+  clickMenu();
+}
